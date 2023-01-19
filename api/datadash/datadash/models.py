c67f0fcad6031963f59814f87fcbae3d671ec24e
Fred Snyder <safe.sand1626@fastmail.com>
2023-01-19 15:51:01 +0100

    Add comments

--- api/datadash/datadash/models.py
+++ api/datadash/datadash/models.py
@@ -11,3 +11,10 @@ class EuroToDollar(models.Model):
 
 # whenever modifying db-models run:
 # python manage.py makemigrations
+# python manage.py migrate
+
+# once the app is published and live
+# check in the migration files to Git
+# in production: use those migrations to migrate the db
+# no need to make migrations on production machines
+# https://stackoverflow.com/questions/28035119/should-i-be-adding-the-django-migration-files-in-the-gitignore-file
