e2b4ea3a07b808e3d142de003f84541c0714c44e
Fred Snyder <safe.sand1626@fastmail.com>
2023-01-22 17:27:43 +0100

    Refactor footer, use MUI components

--- app/src/components/interface/Footer.jsx
+++ app/src/components/interface/Footer.jsx
@@ -1,14 +1,10 @@
+import { Container } from '@mui/material';
 import { ExternalLink } from '@/components/utils/ExternalLink';
 
 export const Footer = () => {
 	return (
-		<footer className="container mx-auto p-5">
-			<ExternalLink src="Test">Kaboom Shebang</ExternalLink>
-			<img
-				className="fixed w-20 bottom-5 right-0"
-				src="https://assets.kbsb.app/svg/shebang_3d_for_web.svg"
-				alt="Shebang"
-			/>
-		</footer>
+		<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+			<ExternalLink src="Test">KBSB</ExternalLink>
+		</Container>
 	);
 };
