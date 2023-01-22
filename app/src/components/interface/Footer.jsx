67249bb7ed92775e94cef63525fbdd577a32195e
Fred Snyder <safe.sand1626@fastmail.com>
2023-01-23 00:05:16 +0100

    Add copyright

--- app/src/components/interface/Footer.jsx
+++ app/src/components/interface/Footer.jsx
@@ -2,9 +2,11 @@ import { Container } from '@mui/material';
 import { ExternalLink } from '@/components/utils/ExternalLink';
 
 export const Footer = () => {
+	const date = new Date();
+
 	return (
 		<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-			<ExternalLink src="Test">KBSB</ExternalLink>
+			<ExternalLink src="Test">&#169; {date.getFullYear()} KBSB</ExternalLink>
 		</Container>
 	);
 };
