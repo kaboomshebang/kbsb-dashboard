455135ba50349c0a53ecd9d98cb3948ec3964cf9
Fred Snyder <safe.sand1626@fastmail.com>
2023-01-23 00:06:21 +0100

    Refactor, fetch data from API, add form scaffold

--- app/src/components/dashboard/apexcharts/EuroDollar.jsx
+++ app/src/components/dashboard/apexcharts/EuroDollar.jsx
@@ -1,27 +1,69 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+// MUI components
+import { Box, Checkbox, FormControlLabel, FormGroup, Button, Typography } from '@mui/material';
+
+// MUI icons
+import SendIcon from '@mui/icons-material/Send';
+
+// ApexCharts
 import Chart from 'react-apexcharts';
 
-export const EuroDollar = () => {
-	const [options, setOptions] = useState({
-		chart: {
-			id: 'basic-bar',
-		},
-		xaxis: {
-			categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-		},
-	});
+const endpoint = 'http://100.111.214.8:8000/api/eurodollars/';
 
-	const [series, setseries] = useState([
-		{
-			name: 'series-1',
-			data: [30, 40, 45, 50, 49, 60, 70, 91],
-		},
-	]);
+export const EuroDollar = () => {
+	const [options, setOptions] = useState({});
+	const [series, setSeries] = useState([]);
+
+	useEffect(() => {
+		fetch(endpoint)
+			.then((res) => res.json())
+			.then((data) => {
+				setOptions(() => {
+					return {
+						chart: {
+							id: 'euro-dollar',
+						},
+						xaxis: {
+							categories: data.map((d) => d.date),
+						},
+					};
+				});
+				setSeries(() => {
+					return [
+						{
+							name: 'dates',
+							data: data.map((d) => d.priceDollar),
+						},
+					];
+				});
+			});
+	}, []);
+
+	const addNewPrice = () => {
+		// TODO: add POST logic
+	};
 
 	return (
-		<article>
-			<h1>Test</h1>
+		<Box component="article">
+			<Typography variant="h6" component="h1">
+				EuroDollar price
+			</Typography>
 			<Chart options={options} series={series} type="bar" width="500" />
-		</article>
+			<FormGroup>
+				<FormControlLabel
+					control={<Checkbox defaultChecked />}
+					label="Todo: add POST functionality"
+				/>
+				<FormControlLabel
+					disabled
+					control={<Checkbox />}
+					label="Todo: add more API/HTTP/CRUD operations"
+				/>
+				<Button variant="contained" color="primary" endIcon={<SendIcon />}>
+					Todo: Add new price
+				</Button>
+			</FormGroup>
+		</Box>
 	);
 };
