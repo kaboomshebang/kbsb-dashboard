8b9ccfbce8a601ebe9525636e6aaa5b87a265dec
Fred Snyder <safe.sand1626@fastmail.com>
2023-01-22 17:27:15 +0100

    Import color from lib

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,9 +4,7 @@ import { useState } from 'react';
 import { Box } from '@mui/system';
 import { Container, Grid, Paper } from '@mui/material';
 import { CssBaseline } from '@mui/material';
-
-// Charts examples
-import { EuroDollar } from '@/components/EuroDollar';
+import { grey } from '@mui/material/colors';
 
 // interface
 import { NavBar } from '@/components/interface/NavBar';
@@ -22,7 +20,7 @@ export const App = () => {
 			<Box
 				component="main"
 				sx={{
-					backgroundColor: '#f5f5f5',
+					backgroundColor: grey[100],
 					flexGrow: 1,
 					height: '100vh',
 					overflow: 'auto',
