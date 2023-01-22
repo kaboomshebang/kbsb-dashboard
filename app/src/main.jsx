7e80ec83025d7db9405c8a6d542fe05ab29b5cd4
Fred Snyder <safe.sand1626@fastmail.com>
2023-01-22 17:26:15 +0100

    Config global theme settings

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+// MUI
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+	typography: {
+		fontFamily: ['Public Sans', 'sans-serif'].join(','),
+	},
+});
+
+// component imports
+import { App } from '@/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
