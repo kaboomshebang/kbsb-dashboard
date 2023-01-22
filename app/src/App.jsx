b1388c8058647e75fbd40f58b47d00ae6efb3039
Fred Snyder <safe.sand1626@fastmail.com>
2023-01-22 17:29:11 +0100

    Refactor structure, split into multiple components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from 'react';
-
 // MUI components
 import { Box } from '@mui/system';
-import { Container, Grid, Paper } from '@mui/material';
 import { CssBaseline } from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 // interface
 import { NavBar } from '@/components/interface/NavBar';
 import { Header } from '@/components/interface/Header';
+import { Dashboard } from '@/components/dashboard/Dashboard';
+import { Footer } from '@/components/interface/Footer';
 
 export const App = () => {
 	return (
 		<>
 			<CssBaseline />
-
-			<NavBar />
 
 			<Box
 				component="main"
@@ -26,39 +23,12 @@ export const App = () => {
 					overflow: 'auto',
 				}}
 			>
+				<NavBar />
 				<Header />
 
-				{/* Dashboard */}
-
-				<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-					<Grid container spacing={3}>
-						<Grid item xs={12} md={8} lg={9}>
-							<Paper
-								sx={{
-									p: 2,
-									display: 'flex',
-									flexDirection: 'column',
-								}}
-							>
-								<EuroDollar />
-							</Paper>
-						</Grid>
-						<Grid item xs={12} md={8} lg={9}>
-							<Paper
-								sx={{
-									p: 2,
-									display: 'flex',
-									flexDirection: 'column',
-								}}
-							>
-								<EuroDollar />
-							</Paper>
-						</Grid>
-					</Grid>
-				</Container>
+				<Dashboard />
+				<Footer />
 			</Box>
-
-			{/* Footer */}
 		</>
 	);
 };
---
+++ app/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { Container, Grid, Paper } from '@mui/material';
+
+// Charts examples
+import { EuroDollar } from '@/components/EuroDollar';
+
+export const Dashboard = () => {
+	return (
+		<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+			<Grid container spacing={3}>
+				<Grid item xs={12}>
+					<Paper
+						sx={{
+							p: 4,
+							display: 'flex',
+							flexDirection: 'column',
+						}}
+					>
+						<EuroDollar />
+					</Paper>
+				</Grid>
+			</Grid>
+		</Container>
+	);
+};
